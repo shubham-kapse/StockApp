c45dd04d2b41aa0e796f7117351c0074ff27c179
unknown <you@example.com>
2025-09-28 21:43:02 +0530

    implemented text-gray-400

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -28,7 +28,7 @@ const UserDropdown = () => {
       <DropdownMenuTrigger asChild>
         <Button
           variant="ghost"
-          className="flex items-center gap-3 text-gray-4 hover:text-yellow-500"
+          className="flex items-center gap-3 text-gray-400 hover:text-yellow-500"
         >
           <Avatar className="w-8 h-8">
             <AvatarImage src="https://github.com/shadcn.png" />
